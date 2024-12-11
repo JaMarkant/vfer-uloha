45d55d45834c1c22cf0238661edbccc7a3b8e649
Jachym Markant <jachym.markant@ibillboard.com>
2024-12-11 13:12:51 +0100

    working

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ cp ./api/.env.dist ./api/.env
 
 docker compose build
 
+#REVIEW: Why npm install, when it's done in docker file?
 docker compose run api npm install
 
+#REVIEW: Why force-recreate?
 docker compose up --force-recreate
